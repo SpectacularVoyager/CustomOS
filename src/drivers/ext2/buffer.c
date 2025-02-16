d089fa7746bb6b18036902dd0f5800aae5f3d26c
ankush <Mail2ankushd@gmail.com>
2025-02-17 00:35:48 +0530

    fixed bud in ext2 buffer

--- src/drivers/ext2/buffer.c
+++ src/drivers/ext2/buffer.c
@@ -1,6 +1,7 @@
 #include "buffer.h"
 #include "drivers/ahci/ahci.h"
 #include "stdlib/string.h"
+#include "stdlib/stdio.h"
 #include "utils/utils.h"
 //TODO: TEST
 //
@@ -20,17 +21,47 @@ void EXT2_BUFFER_SKIP(EXT2_BUFFER* buffer,int len){
 		buffer->pointer+=part;
 	}
 }
-void EXT2_BUFFER_READ(EXT2_BUFFER* buffer,void* data,int len){
-	if(buffer->pointer+len<EXT2_BUFF_CAP){
+void EXT_SIMPLE_BUFFERED_READ(EXT2_BUFFER* buffer,void* data,int len){
 		memcpy(data,buffer->buffer+buffer->pointer,len);
 		buffer->pointer+=len;
+}
+void EXT2_BUFFER_READ(EXT2_BUFFER* buffer,void* data,int len){
+	// if(buffer->pointer+len<EXT2_BUFF_CAP){
+	// 	memcpy(data,buffer->buffer+buffer->pointer,len);
+	// 	buffer->pointer+=len;
+	// }else{
+	// 	unsigned int part=EXT2_BUFF_CAP-buffer->pointer-1;
+	// 	memcpy(data,buffer->buffer,part);
+	// 	EXT2_REFRESH(buffer);
+	// 	buffer->pointer=0;
+	// 	memcpy(data+part,buffer->buffer+buffer->pointer,len-part);
+	// 	buffer->pointer+=part;
+	// }
+	int print=len>1000?1:0;
+	if(print) printf("READING data[%x]\t%x\n",len,buffer->pointer);
+	if(buffer->pointer+len<EXT2_BUFF_CAP){
+		EXT_SIMPLE_BUFFERED_READ(buffer,data,len);
 	}else{
-		unsigned int part=EXT2_BUFF_CAP-buffer->pointer-1;
-		memcpy(data,buffer->buffer,part);
-		EXT2_REFRESH(buffer);
+		unsigned int part=EXT2_BUFF_CAP-buffer->pointer;
+		EXT_SIMPLE_BUFFERED_READ(buffer,data,part);
+		len-=part;
+		data+=part;
+		FORI(len/EXT2_BUFF_CAP){
+			buffer->pointer=0;
+			EXT2_REFRESH(buffer);
+			if(print) printf("READING data[%x]\t%x\t%p\n",len,buffer->pointer,data);
+			memcpy(data,buffer->buffer,EXT2_BUFF_CAP);
+			data+=EXT2_BUFF_CAP;
+			len-=EXT2_BUFF_CAP;
+		}
+		if(print) printf("READING data[%x]\t%x\t%p\n",len,buffer->pointer,data);
 		buffer->pointer=0;
-		memcpy(data+part,buffer->buffer+buffer->pointer,len-part);
-		buffer->pointer+=part;
+		EXT2_REFRESH(buffer);
+		EXT_SIMPLE_BUFFERED_READ(buffer,data,len);
+		hexdump(data,40,40);
+		//EXT2_BUFFER_READ(buffer,data,len);
+		printf("DONE\n");
+
 	}
 }
 void EXT2_BUFFER_INIT(EXT2_BUFFER* buffer,AHCI_HBA_PORT* port,uint64_t lba){
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -23,3 +23,21 @@ void hexdump(void* mem,size_t size,size_t stride){
 	}
 	printf("\n");
 }
+void khexdump(void* mem,size_t size,size_t stride){
+	uint8_t* mat=mem;
+	int x=0;
+	FORI(size/stride){
+		kprintf("0x%02x\t",stride*x);
+		FORJ(stride){
+			kprintf("%02x ",mat[stride*i+j]);
+		}
+		kprintf("\n");
+			x++;
+	}
+	kprintf("0x%02x\t",stride*x);
+	FORI(size%stride){
+		x++;
+		kprintf("%02x ",mat[size/stride+i]);
+	}
+	kprintf("\n");
+}
