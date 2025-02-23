cb56d610f6d4e295d4902faf2c2262a27dcfc37e
ankush <Mail2ankushd@gmail.com>
2025-02-23 19:51:06 +0530

    Better context switches

--- src/core/user.asm
+++ src/core/user.asm
@@ -35,10 +35,26 @@ USER_JUMP_ASM:
 	mov es,ax
 
     ; Build a fake iret frame
-	push rax							; 
+	push rax							;
 	push rdx							; USER STACK
 	push 0x202|0x3000					; RFLAGS INT ENABLE AND RESERVED
 	push 0x18 | USER_PREV				; Selector
 	push rsi							; ENTRY POINT
 
+	mov r15,[rdi+0x8]
+	mov r14,[rdi+0x10]
+	mov r13,[rdi+0x18]
+	mov r12,[rdi+0x20]
+	mov r11,[rdi+0x28]
+	mov r10,[rdi+0x30]
+	mov r9 ,[rdi+0x38]
+	mov r8 ,[rdi+0x40]
+	mov rbp,[rdi+0x48]
+	mov rsi,[rdi+0x58]
+	mov rdx,[rdi+0x60]
+	mov rcx,[rdi+0x68]
+	mov rbx,[rdi+0x70]
+	mov rax,[rdi+0x78]
+
+	mov rdi,[rdi+0x50]
 	iretq
--- src/kernel.c
+++ src/kernel.c
@@ -243,23 +243,23 @@ void kernel_main(unsigned long multiboot_address,int magic,int cs,unsigned long 
 	Scheduler_START();
 	//evaluate("/usr/bin/test ");
 
-	ClearScreen();
-	TERM_SET_POS(0,0);
-	EXT2_INODE elf;
-	if(EXT2_GET_INODE_FROM_PATH(&elf,"/usr/bin/cat")==1){
-		char* hex=malloc(elf.size);
-		EXT2_READFILE(&elf,hex,elf.size);
-		ELF_FILE file;
-		int s=ELF_PARSE(&file,hex,elf.size);
-		if(s==1){
-			char* args[]={"/usr/bin/cat","/home/ASM/main.c",0};
-			USERMODE_EXEC_ELF(&file,args,NULL);
-		}else{
-			printf("FILE NOT EXECUTABLE [%s]\n",errno_ELF(s));
-		}
-		//LOGVAL(elf.size);
-		//khexdump(hex,elf.size,32);
-	}
+	// ClearScreen();
+	// TERM_SET_POS(0,0);
+	// EXT2_INODE elf;
+	// if(EXT2_GET_INODE_FROM_PATH(&elf,"/usr/bin/cat")==1){
+	// 	char* hex=malloc(elf.size);
+	// 	EXT2_READFILE(&elf,hex,elf.size);
+	// 	ELF_FILE file;
+	// 	int s=ELF_PARSE(&file,hex,elf.size);
+	// 	if(s==1){
+	// 		char* args[]={"/usr/bin/cat","/home/ASM/hello.asm",0};
+	// 		USERMODE_EXEC_ELF(&file,args,NULL);
+	// 	}else{
+	// 		printf("FILE NOT EXECUTABLE [%s]\n",errno_ELF(s));
+	// 	}
+	// 	//LOGVAL(elf.size);
+	// 	//khexdump(hex,elf.size,32);
+	// }
 
 	//USERMODE_ADD();
 	//USERMODE_ADD();
